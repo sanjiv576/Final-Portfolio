4afd7fe1a2e2efbf0c610bc4e2df595547a812e3
sanjiv576 <shresthasanjiv576@gmail.com>
2024-05-23 21:28:24 +0545

    Updates

--- src/utils/inputSanitization.js
+++
@@ -1,7 +0,0 @@
-  // sanitizing the inputs to prevent from XSS 
-  const sanitizeInput = (input) => {
-    // Use DOMParser to convert input to text
-    const doc = new DOMParser().parseFromString(input, 'text/html');
-    return doc.body.textContent || "";
-};
-export default sanitizeInput;
