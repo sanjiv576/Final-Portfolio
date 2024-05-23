4f5f8e55484e81d48fd87f17db115d9fbb49b29e
sanjiv576 <shresthasanjiv576@gmail.com>
2024-05-23 21:26:47 +0545

    add cv download feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,81 @@ import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { styles } from '../style';
 import { navLinks } from '../constants/index';
 import { backend, menu, close, creator, mylogo } from '../assets';
-
+import { Snackbar, Tooltip } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
+import sound from '../assets/music/sound.wav';
+import { MySnackbar } from './MySnackBar';
+import cvPath from '../assets/cv/cv_underconstruction.pdf'
 const Navbar = () => {
 
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(true);
+  const [snack, setSnack] = useState({
+    type: '',
+    message: '',
+  });
+
+  const play = () => new Audio(sound).play();
+
+
+  // for open and close snackbar
+  const [open, setOpen] = React.useState(false);
+
+  // for closing snackbar
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  // download CV pdf file
+  const handleDownload = async (e) => {
+    e.preventDefault();
+
+    const confirmation = window.confirm('Are you sure you want to download the CV?');
+    if (confirmation) {
+      try {
+        const response = await fetch(cvPath);
+        if (!response.ok) {
+          // throw new Error('Network response was not ok');
+          setSnack({
+            type: 'error',
+            message: 'Network response was not ok',
+          });
+          setOpen(true);
+        }
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'Sanjiv Shrestha Resume.pdf';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+
+        // Call the play function to play the music
+        play();
+
+        // Set the snack state to show success message
+        setSnack({
+          type: 'success',
+          message: 'CV is ready to download.',
+        });
+        setOpen(true);
+      } catch (error) {
+        console.error('There was an error downloading the CV:', error);
+
+        // Optionally, set the snack state to show error message
+        setSnack({
+          type: 'error',
+          message: 'Failed to download CV.',
+        });
+        setOpen(true);
+      }
+    }
+  };
 
   const navigate = useNavigate();
   return (
@@ -32,6 +102,17 @@ const Navbar = () => {
           <p className='text-white text-[16px] font-bold cursor-pointer flex'>Sanjiv Shrestha &nbsp;
             <span className='sm:block hidden text-[16x]'> | &nbsp; Portfolio</span></p>
         </Link>
+
+        <Tooltip title="Just click to down CV.">
+          <a
+            href="#"
+            className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md btn-outline btn-info"
+            onClick={handleDownload}>
+            Download CV <span><DownloadIcon /></span>
+          </a>
+        </Tooltip>
+        <MySnackbar open={open} handleClose={handleClose} type={snack.type} message={snack.message} />
+
         <ul className='list-none flex-row hidden sm:flex gap-10'>
 
           {
@@ -48,8 +129,8 @@ const Navbar = () => {
 
                 <NavLink to={`/${link.id}`}>{link.title}</NavLink>
 
-                
-                </li>
+
+              </li>
             ))
           }
 
@@ -99,6 +180,7 @@ const Navbar = () => {
         </div>
 
       </div>
+
     </nav>
   )
 }
